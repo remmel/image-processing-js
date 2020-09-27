fc974bf80071022a47950890b18283b8a41dadc4
Remy Mellet <remy.meller@gmail.com>
2020-09-27 23:57:07 +0200

    Laplace in opencv

--- script-opencv.js
+++ script-opencv.js
@@ -1,8 +1,8 @@
 
 function openCvReady() {
     var img = new Image();
-// img.src = 'rhino.jpg';
-    img.src = 'HSV_color_solid_cylinder.png';
+img.src = 'rhino.jpg';
+    // img.src = 'HSV_color_solid_cylinder.png';
 
     img.onload = function () {
         var canvasOriginal = document.getElementById('original');
@@ -15,15 +15,21 @@ function openCvReady() {
         let grayscaleMat = new cv.Mat();
         let hsvMat = new cv.Mat();
         let hsvTestMat = new cv.Mat(); //RGB -> HSV -> RGB
+        let laplaceMat = new cv.Mat();
 
         cv.cvtColor(src, grayscaleMat, cv.COLOR_RGBA2GRAY, 0);
         cv.cvtColor(src, hsvMat, cv.COLOR_RGB2HSV, 0);
         cv.cvtColor(hsvMat, hsvTestMat, cv.COLOR_HSV2RGB, 0);
 
+        cv.cvtColor(src, src, cv.COLOR_RGB2GRAY, 0);
+        cv.Laplacian(src, laplaceMat, cv.CV_8U, 1, 2, 0, cv.BORDER_DEFAULT);
+
+
 
         cv.imshow('grayscale', grayscaleMat);
         cv.imshow('hsvTest', hsvTestMat);
         cv.imshow('hsv', hsvMat);
+        cv.imshow('laplace', laplaceMat)
         src.delete();
         grayscaleMat.delete();
         hsvMat.delete();
